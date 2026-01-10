19720e8abf3ad5f08adf392c898227871a0ce303
toms74209200 <motomotomato@gmail.com>
2026-01-10 21:41:14 +0900

    :sparkles: Update DB schema

--- database/schema/like_events.sql
+++ database/schema/like_events.sql
@@ -9,5 +9,3 @@ CREATE TABLE like_events (
 CREATE INDEX idx_like_events_post_id ON like_events (post_id);
 CREATE INDEX idx_like_events_user_id ON like_events (user_id);
 CREATE INDEX idx_like_events_post_user ON like_events (post_id, user_id);
-CREATE INDEX idx_like_events_occurred_at ON like_events (occurred_at);
-CREATE INDEX idx_like_events_event_type ON like_events (event_type);
