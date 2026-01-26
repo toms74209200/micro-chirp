3cde289491c55777ff79079f32ed1bd0db96879e
toms74209200 <motomotomato@gmail.com>
2026-01-27 07:40:15 +0900

    :white_check_mark: Add load test for reply API

--- load-test/locustfile.py
+++ load-test/locustfile.py
@@ -1,4 +1,12 @@
 from scenarios.posts import CreatePostAPI, GetPostAPI, DeletePostAPI
 from scenarios.likes import LikePostAPI, UnlikePostAPI
+from scenarios.replies import ReplyPostAPI
 
-__all__ = ["CreatePostAPI", "GetPostAPI", "DeletePostAPI", "LikePostAPI", "UnlikePostAPI"]
+__all__ = [
+    "CreatePostAPI",
+    "GetPostAPI",
+    "DeletePostAPI",
+    "LikePostAPI",
+    "UnlikePostAPI",
+    "ReplyPostAPI",
+]
